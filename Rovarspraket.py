dc2904dc1fa82b53ed5c41390d6293f43d5c73c8
sng0616 <sarahngrant0691@gmail.com>
2015-05-02 16:00:36 -0400

    Added considerations for some non-ASCII characters; created variables to store ASCII & non-ASCII vowel characters; changed first loop to remove enumeration where unnecessary; and created loop to decode Rovarspraket

--- Rovarspraket.py
+++ Rovarspraket.py
@@ -1,17 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 # Source: http://www.reddit.com/r/dailyprogrammer/comments/341c03/20150427_challenge_212_easy_r%C3%B6varspr%C3%A5ket/
 
-phrase = "Purple"
-enume = enumerate(phrase)
+phrase = u"Tre Kronor är världens bästa ishockeylag."
+vowels = "AaEeIiOoUuYy"
+vowels2 = u"\xc4\xc5\xe4\xe5\xd6\xf6"
+punct = ".?!:,@#$%^&*()[]{}<>`~"
 extra = ""
 rovar = ""
 
-for i, j in enume:
-	if j in 'aeiouy':
+#print vowels, vowels2
+
+for j in phrase:
+# Taking vowels and spaces between words into account.
+	if j in vowels or j in vowels2 or j in punct or j == " ":
 		rovar = rovar + j
 	else: 
 		extra = j + "o" + j
 		rovar = rovar + extra
+		
 print rovar
 
-# To-do list:
-# 1. Add considerations for non-ASCII characters
+# Decode Rovarspraket
+
+phrase_rovar = "ToTrore KoKrorononoror äror vovärorloldodenonsos bobäsostota isoshohocockokeylolagog."
+enume_rovar = enumerate(phrase_rovar)
+extra_rovar = ""
+derovar = ""
+length_rovar = len(phrase_rovar)
+start = 0
+indices = []
+
+for x, y in enume_rovar:
+#	print x, y
+	if y == "o" and phrase_rovar[x] + phrase_rovar[x+1] == "o" + phrase_rovar[x-1]:
+		enume_rovar.next()
+		continue		
+	else: 
+		derovar = derovar + y
+print derovar
